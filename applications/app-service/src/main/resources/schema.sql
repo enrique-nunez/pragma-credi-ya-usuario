4447774ca00e6603e8ffcfca3869bed9059e4f9e
Enrique Nuñez <j.nunez.constantino@gmail.com>
2025-09-17 03:36:15 -0500

    feat: configuraciones del microservicio de usuario para el container

--- applications/app-service/src/main/resources/schema.sql
+++ applications/app-service/src/main/resources/schema.sql
@@ -1,28 +1,28 @@
--- Crear tabla roles
-CREATE TABLE IF NOT EXISTS rol (
-    id BIGSERIAL PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL UNIQUE,
-    descripcion VARCHAR(255)
-);
+-- -- Crear tabla roles
+-- CREATE TABLE IF NOT EXISTS rol (
+--     id BIGSERIAL PRIMARY KEY,
+--     nombre VARCHAR(50) NOT NULL UNIQUE,
+--     descripcion VARCHAR(255)
+-- );
 
--- Insertar roles por defecto
-INSERT INTO rol (nombre, descripcion) VALUES
-('ADMIN', 'Administrador del sistema'),
-('USER', 'Usuario estándar'),
-('MANAGER', 'Gerente')
-ON CONFLICT (nombre) DO NOTHING;
+-- -- Insertar roles por defecto
+-- INSERT INTO rol (nombre, descripcion) VALUES
+-- ('ADMIN', 'Administrador del sistema'),
+-- ('USER', 'Usuario estándar'),
+-- ('MANAGER', 'Gerente')
+-- ON CONFLICT (nombre) DO NOTHING;
 
--- Crear tabla usuario
-CREATE TABLE IF NOT EXISTS usuario (
-    id BIGSERIAL PRIMARY KEY,
-    nombres VARCHAR(100) NOT NULL,
-    apellidos VARCHAR(100) NOT NULL,
-    fecha_nacimiento DATE NOT NULL,
-    direccion VARCHAR(255),
-    telefono VARCHAR(20),
-    correo_electronico VARCHAR(150) UNIQUE NOT NULL,
-    salario_base DECIMAL(15,2),
-    password VARCHAR(255) NOT NULL,
-    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    role_id BIGINT REFERENCES rol(id) DEFAULT 2
-);
+-- -- Crear tabla usuario
+-- CREATE TABLE IF NOT EXISTS usuario (
+--     id BIGSERIAL PRIMARY KEY,
+--     nombres VARCHAR(100) NOT NULL,
+--     apellidos VARCHAR(100) NOT NULL,
+--     fecha_nacimiento DATE NOT NULL,
+--     direccion VARCHAR(255),
+--     telefono VARCHAR(20),
+--     correo_electronico VARCHAR(150) UNIQUE NOT NULL,
+--     salario_base DECIMAL(15,2),
+--     password VARCHAR(255) NOT NULL,
+--     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     role_id BIGINT REFERENCES rol(id) DEFAULT 2
+-- );
